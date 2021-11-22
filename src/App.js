ddaaa89f632731d5f6d5f902c54ab8683ab988ae
BC-macair010 <bc-macair010@BC-macair010s-MacBook-Air.local>
2021-11-22 14:32:09 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+componentDidMount () {
+  axios.get("https://restcountries.com/v3.1/all")
+  .then((res) => {
+    this.setState({data: res.data});
+  });
+}
+
+  render() {
+    return (
+      <div className="countries">
+       {this.state.data.map((c) => (
+       <div className="country" key={c.name.common}>
+         <h2>{c.name.common}</h2> <p>{c.capital}</p>
+      <img src={c.flags.svg} />
+      </div>
+       ))}
+      </div>
+    );
+  }
 }
 
 export default App;
